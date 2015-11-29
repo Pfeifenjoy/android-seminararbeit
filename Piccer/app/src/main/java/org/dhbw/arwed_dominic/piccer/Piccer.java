fc0b55dc228928a334befa3c817c6ef44bb740f2
Arwed Mett <arwed.mett@googlemail.com>
2015-11-29 12:19:33 +0100

    started technical Dokumentation

--- Piccer/app/src/main/java/org/dhbw/arwed_dominic/piccer/Piccer.java
+++ Piccer/app/src/main/java/org/dhbw/arwed_dominic/piccer/Piccer.java
@@ -1,5 +1,6 @@
 package org.dhbw.arwed_dominic.piccer;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.util.Log;
